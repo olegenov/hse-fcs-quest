f26d3d93a5072d4e524282dc3116034084d8ecbd
olegenov <nik_kitaev@mail.ru>
2023-03-27 00:37:08 +0300

    fck static

--- quest/entrypoint.sh
+++ quest/entrypoint.sh
@@ -5,6 +5,6 @@ sleep 10
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createcachetable
-python manage.py collectstatic  --noinput
+python manage.py collectstatic --noinput --clear
 
 exec "$@"
--- quest/quest/settings.py
+++ quest/quest/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
